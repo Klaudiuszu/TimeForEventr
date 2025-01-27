e3b7bc48aa5f47a8b74c9f1ee8bcd512116a3d8e
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2025-01-27 23:02:10 +0100

    002 adjust blog

---
+++ app/lib/interface.ts
@@ -0,0 +1,13 @@
+export interface simpleBlogType {
+    title: string;
+    smallDescription: string;
+    currentSlug: string;
+    titleImage: any;
+  }
+  
+  export interface fullBlogType {
+    currentSlug: string;
+    title: string;
+    content: any;
+    titleImage: any;
+  }
