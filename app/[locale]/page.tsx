f36fdf81ac56622ae4a4e135f2229ab87baa8691
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2025-03-24 19:20:40 +0100

    fix locale

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,4 @@
 import { getTranslations } from "next-intl/server";
-import { ThemeProvider } from "../components/theme-provider";
 import Navbar from "../components/Navbar";
 import { getMessages } from "next-intl/server";
 import '../globals.css';
@@ -10,12 +9,7 @@ export default async function Home({ params }: { params: { locale: string } }) {
   const messages = await getMessages({ locale });
 
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
+    <>
       <Navbar locale={locale} messages={messages as Record<string, string>} />
       <main className="max-w-2xl mx-auto px-4 py-4 xl:py-12">
         <h1 className="text-3xl font-bold text-center my-8">
@@ -25,6 +19,6 @@ export default async function Home({ params }: { params: { locale: string } }) {
           {t("description")}
         </p>
       </main>
-    </ThemeProvider>
+    </>
   );
 }
